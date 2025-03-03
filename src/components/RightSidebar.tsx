4926ae9ad40c25e8a99ef61acaf91b05cc612ac5
Harsh-Tank-22 <htank@biasafe.ai>
2025-03-03 18:00:06 -0500

    types added

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,23 +1,31 @@
 import { FaPlus } from "react-icons/fa";
 import { BsReverseLayoutSidebarReverse } from "react-icons/bs";
-import TradingImg from "../assets/trading.png"
+import TradingImg from "../assets/trading.png";
 
-const RightSidebar = ({setRightexpanded}) => {
+type RightSidebarProps = {
+  setRightexpanded: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const RightSidebar: React.FC<RightSidebarProps> = ({ setRightexpanded }) => {
   return (
     <div className='w-[25%] h-screen m-6 rounded-2xl shadow-lg bg-gray-100 flex flex-col gap-4'>
-        <div className='flex justify-between mx-7 my-4'>
-            <div className='flex text-purple-700 gap-2'><FaPlus size={25} className='text-purple-700'/><p>Add</p></div>
-            <div><button onClick={()=>setRightexpanded((prev)=>!prev)}>
-                <BsReverseLayoutSidebarReverse size={25}/>
-            </button></div>
-            
+      <div className='flex justify-between mx-7 my-4'>
+        <div className='flex text-purple-700 gap-2'>
+          <FaPlus size={25} className='text-purple-700' />
+          <p>Add</p>
         </div>
-        <div className='h-[40%] bg-white mx-4 rounded-md'></div>
-        <div className='h-[40%] bg-white mx-4 rounded-md'>
-            <img src={TradingImg} className='w-full h-full object-contain'></img>
+        <div>
+          <button onClick={() => setRightexpanded((prev) => !prev)}>
+            <BsReverseLayoutSidebarReverse size={25} />
+          </button>
         </div>
+      </div>
+      <div className='h-[40%] bg-white mx-4 rounded-md'></div>
+      <div className='h-[40%] bg-white mx-4 rounded-md'>
+        <img src={TradingImg} className='w-full h-full object-contain' alt="Trading" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
